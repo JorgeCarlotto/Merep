ab1701403aad034696a4a8bad020d7c8e9f1270b
JorgeCarlotto <jorgemcarlotto@gmail.com>
2022-06-02 11:50:12 -0300

    router controler engine ok

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const app = express();
 
+const routerMain = require('./routers/main.js');
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.use('/',routerMain);
+
 app.listen(3001, ()=>console.log('Server running...'))
---
+++ controller/mainControler.js
@@ -0,0 +1,8 @@
+let mainController ={
+
+    index: function(req,res){
+        res.render('main')
+    },
+}
+
+module.exports = mainController;
