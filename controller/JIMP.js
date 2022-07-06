817a87809430887bdb5704642def6c28a2bdf696
JorgeCarlotto <jorgemcarlotto@gmail.com>
2022-07-05 23:00:33 -0300

    Jimp test

---
+++ controller/JIMP.js
@@ -0,0 +1,37 @@
+let Jimp = require('jimp')
+
+
+
+
+// let image = new Jimp(300, 530, 'green', (err, image) => {
+//   if (err) throw err
+// })
+
+ let message = 'Hello!'
+ let x = 50
+ let y = 80
+
+// Jimp.loadFont(Jimp.FONT_SANS_64_BLACK)
+
+//   .then(font => {
+//     image.print(font, x, y, message)
+//     return image
+//   })
+  
+//   .then(image => {
+
+//     let file = `new_name.${image.getExtension()}`
+
+//     return image.write(file) // save
+//   })
+
+
+
+
+
+
+  // Jimp.loadFont('../public/img/1657059249646_DSC_4337.JPG').then(font => {
+  //   // load font from .fnt file
+  //   image.print(font, x, y, message); // print a message on an image. message can be a any type
+  //   image.print(font, x, y, message, maxWidth); // print a message on an image with text wrapped at maxWidth
+  // });
